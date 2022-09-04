eebb9e961b863439ecca3bf8a2365082733df45e
strapiAdmin <dev@weebut.com>
2022-09-04 13:38:15 +0000

    (strapi_console) deployment #19: triggered at 2022-09-04-22-38-08

--- strapi/src/api/hello-world/routes/hello-world.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * hello-world router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::hello-world.hello-world');
--- strapi/src/api/hello-world/services/hello-world.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * hello-world service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::hello-world.hello-world');
