c774ad433c8dd759148a65a500c68ac92d471617
Jaemin Kim <44409248+JaeminBest@users.noreply.github.com>
2022-09-04 17:45:26 +0900

    Bug/cms (#5)
    * done
    
    * done
    
    * done
    
    * done
    
    * done
    
    * (strapi_console) deployment #13: triggered at 2022-09-04-13-59-37
    
    * done
    
    * done
    
    * done
    
    * (strapi_console) deployment #15: triggered at 2022-09-04-14-31-55
    
    * done
    
    * (strapi_console) deployment #16: triggered at 2022-09-04-15-16-03
    
    * done
    
    * (strapi_console) deployment #17: triggered at 2022-09-04-15-50-14
    
    * done
    
    * (strapi_console) deployment #18: triggered at 2022-09-04-17-17-50
    
    * done
    
    * done
    
    Co-authored-by: strapiAdmin <dev@weebut.com>

--- strapi/ecosystem.config.js
+++ strapi/ecosystem.config.js
@@ -4,9 +4,10 @@ module.exports = {
       name: 'init-cms', // Your project name
       cwd: '/opt/app', // Path to your project
       script: 'npm', // For this example we're using npm, could also be yarn
-      args: 'start', // Script to start the Strapi server, `start` by default
+      args: 'run pm2start', // Script to start the Strapi server, `start` by default
       env: {
         NODE_ENV: 'production',
+        PORT: 1338,
       },
     },
   ],
